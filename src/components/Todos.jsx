7092ef7a2d89c970f7d81fd118a10a3fb9e9b138
jorgeluismorales <53586348+jorgeluismorales@users.noreply.github.com>
2021-05-30 23:35:49 -0300

    crud usando api

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -7,40 +7,34 @@ export const Todos = () => {
   const [todo, setTodo] = useState({
     puesto: "",
     empresa: "",
-    descripcion: "",
+    descripcion: ""
   });
   const [todos, setTodos] = useState([]);
   const [error, setError] = useState(false);
   const [companies, setCcompanies] = useState([]);
   const [selectedCompany, setSelectedCompany] = useState("");
   const [editionMode, setEditionMode] = useState(false);
-  const [id, setId] = useState("");
+  const [id, setId] = useState('');
 
   const { puesto, empresa, descripcion } = todo;
 
   useEffect(() => {
     const getData = async () => {
-      const res = await axios.get(
-        "https://api-fake-pilar-tecno.herokuapp.com/organizations?_expand=place"
-      );
-      const jobs = await axios.get(
-        "https://api-fake-pilar-tecno.herokuapp.com/jobs?_expand=organization"
-      );
-      setTodos(jobs.data);
-      setCcompanies(res.data);
-    };
-    getData();
+      const res = await axios.get('https://api-fake-pilar-tecno.herokuapp.com/organizations?_expand=place')
+      const jobs = await axios.get('https://api-fake-pilar-tecno.herokuapp.com/jobs?_expand=organization')
+      setTodos(jobs.data)
+      setCcompanies(res.data)
+    }
+    getData()
   }, []);
 
   useEffect(() => {
     const fetchCompany = async () => {
-      const response = await axios.get(
-        `https://api-fake-pilar-tecno.herokuapp.com/organizations/${empresa}`
-      );
-      setSelectedCompany(response.data.name);
-    };
-    fetchCompany();
-  }, [empresa]);
+      const response = await axios.get(`https://api-fake-pilar-tecno.herokuapp.com/organizations/${empresa}`)
+      setSelectedCompany(response.data.name)
+    }
+    fetchCompany()
+  }, [empresa])
 
   const updateState = (e) => {
     setTodo({
@@ -60,14 +54,11 @@ export const Todos = () => {
       return;
     }
     setError(false);
-    const data = await axios.post(
-      "https://api-fake-pilar-tecno.herokuapp.com/jobs",
-      {
-        position: puesto,
-        description: descripcion,
-        organizationId: empresa,
-      }
-    );
+    const data = await axios.post('https://api-fake-pilar-tecno.herokuapp.com/jobs', {
+      position: puesto,
+      description: descripcion,
+      organizationId: empresa
+    })
     setTodos([
       ...todos,
       {
@@ -75,68 +66,57 @@ export const Todos = () => {
         description: data.data.description,
         id: data.data.id,
         organization: {
-          name: selectedCompany,
-        },
+          name: selectedCompany
+        }
       },
     ]);
     setTodo({
       puesto: "",
       empresa: "",
-      descripcion: "",
+      descripcion: ""
     });
   };
   const handleDelete = (id) => {
-    axios.delete(`https://api-fake-pilar-tecno.herokuapp.com/jobs/${id}`);
+    axios.delete(`https://api-fake-pilar-tecno.herokuapp.com/jobs/${id}`)
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   const enableEdition = (td) => {
-    setEditionMode(true);
+    setEditionMode(true)
     setTodo({
       puesto: td.position,
       empresa: td.organization.id,
-      descripcion: td.description,
-    });
-    setId(td.id);
-    console.log(td);
-  };
+      descripcion: td.description
+    })
+    setId(td.id)
+    console.log(td)
+  }
 
   const edit = async (e) => {
     e.preventDefault();
-    if (puesto.trim() === "" || descripcion.trim() === "") {
+    if (puesto.trim() === "" || empresa.trim() === "" || descripcion.trim() === "") {
       setError(true);
       return;
     }
-    const data = await axios.patch(
-      `https://api-fake-pilar-tecno.herokuapp.com/jobs/${id}`,
-      {
-        position: puesto,
-        description: descripcion,
-        organizationId: empresa,
+    const data = await axios.patch(`https://api-fake-pilar-tecno.herokuapp.com/jobs/${id}`, {
+      position: puesto,
+      description: descripcion,
+      organizationId: empresa
+    })
+    setTodos(todos.map(job => (job.id === id ? {
+      id: data.data.id, position: data.data.position, description: data.data.description, organization: {
+        name: selectedCompany
       }
-    );
-    setTodos(
-      todos.map((job) =>
-        job.id === id
-          ? {
-              id: data.data.id,
-              position: data.data.position,
-              description: data.data.description,
-              organization: {
-                name: selectedCompany,
-              },
-            }
-          : job
-      )
-    );
-    setEditionMode(false);
+
+    } : job)))
+    setEditionMode(false)
     setTodo({
       puesto: "",
       empresa: "",
-      descripcion: "",
+      descripcion: ""
     });
-    setId("");
-  };
+    setId('')
+  }
   return (
     <div className="row">
       <div className="col-6">
@@ -145,24 +125,11 @@ export const Todos = () => {
             Todos los campos son obligatorios
           </div>
         )}
-        <AddTodosForm
-          editionMode={editionMode}
-          edit={edit}
-          handleSubmit={handleSubmit}
-          updateState={updateState}
-          puesto={puesto}
-          descripcion={descripcion}
-          companies={companies}
-        />
+        <AddTodosForm editionMode={editionMode} edit={edit} handleSubmit={handleSubmit} updateState={updateState} puesto={puesto} descripcion={descripcion} companies={companies} />
       </div>
       <div className="col-6">
         {todos.map((td) => (
-          <TodosList
-            td={td}
-            handleDelete={handleDelete}
-            enableEdition={enableEdition}
-            key={td.id}
-          />
+          <TodosList td={td} handleDelete={handleDelete} enableEdition={enableEdition} key={td.id} />
         ))}
       </div>
     </div>
